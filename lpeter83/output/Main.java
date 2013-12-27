7c3111bd354f870e020436722b22d58bff42bf35
unknown <rxu@CHN-RXU.corp.microstrategy.com>
2013-12-27 11:50:38 +0800

    Initial commit

---
+++ lpeter83/output/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.io.PrintStream;
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ * @author lpeter83
+ */
+public class Main {
+	public static void main(String[] args) {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		Scanner in = new Scanner(inputStream);
+		PrintWriter out = new PrintWriter(outputStream);
+		Task solver = new Task();
+		solver.solve(1, in, out);
+		out.close();
+	}
+}
+
+class Task {
+    public void solve(int testNumber, Scanner in, PrintWriter out) {
+        System.out.println("hello");
+    }
+}
+
