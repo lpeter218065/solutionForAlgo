2899b3d4715cddd53f3dfae1966d4cbb1a9ee6aa
rxu <lpeter82218065@gmail.com>
2014-01-02 22:00:28 +0800

    update inputreader and outputwriter

--- algoLib/sai/io/InputReader.java
+++ algoLib/sai/io/InputReader.java
@@ -1,6 +1,8 @@
 package io;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.InputMismatchException;
 
 /**
  * Created by rxu on 12/27/13.
@@ -8,9 +10,70 @@ import java.io.InputStream;
 public class InputReader {
     private InputStream stream;
     private byte[] buf = new byte[1024];
+    private int nc;
+    private int cnt;
 
     public InputReader(InputStream stream) {
         this.stream = stream;
     }
+
+    private int read() {
+        if (nc == -1) {
+            throw new InputMismatchException();
+        }
+        if (cnt >= nc) {
+            cnt = 0;
+            try {
+                nc = stream.read(buf);
+            } catch (IOException e) {
+                throw new InputMismatchException();
+            }
+            if (nc == -1) {
+                return -1;
+            }
+        }
+        return buf[cnt++];
+    }
+
+    private boolean isSpaceChar(int b) {
+        return b == ' ' || b == '\n' || b == -1 || b == '\r' || b == '\t';
+    }
+
+    public int readInt() {
+        int res = 0;
+        boolean sgn = true;
+        int c = read();
+        while (isSpaceChar(c)) c = read();
+        if (c == '-') {
+            sgn = false;
+            c = read();
+        }
+        do {
+            if (c < '0' || c > '9')
+                throw new InputMismatchException();
+            res = res * 10 + (c - '0');
+            c = read();
+        } while (!isSpaceChar(c));
+        return res * (sgn ? 1 : -1);
+    }
+
+    public long readLong() {
+        long res = 0;
+        boolean sgn = true;
+        int c = read();
+        while (isSpaceChar(c)) c = read();
+        if (c == '-') {
+            sgn = false;
+            c = read();
+        }
+        do {
+            if (c < '0' || c > '9')
+                throw new InputMismatchException();
+            res = res * 10 + (c - '0');
+            c = read();
+        } while (!isSpaceChar(c));
+        return res * (sgn ? 1 : -1);
+    }
+
 
 }
--- algoLib/sai/io/OutputWriter.java
+++ algoLib/sai/io/OutputWriter.java
@@ -1,8 +1,43 @@
 package io;
 
+import java.io.*;
+
 /**
  * Created by rxu on 12/27/13.
  */
 public class OutputWriter {
+    private final PrintWriter writer;
 
+    public OutputWriter(OutputStream outputStream) {
+        writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+    }
+
+    public OutputWriter(Writer writer) {
+        this.writer = new PrintWriter(writer);
+    }
+
+    public void print(Object... objects) {
+        for (int i = 0; i < objects.length; i++) {
+            if (i != 0)
+                writer.print(' ');
+            writer.print(objects[i]);
+        }
+    }
+
+    public void printFormat(String format, Object... objects) {
+        writer.printf(format, objects);
+    }
+
+    public void printLine(Object... objects) {
+        print(objects);
+        writer.println();
+    }
+
+    public void close() {
+        writer.close();
+    }
+
+    public void flush() {
+        writer.flush();
+    }
 }
