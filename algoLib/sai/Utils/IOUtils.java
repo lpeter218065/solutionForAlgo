06eeba34a3e49a2a222021ebd57e27440ccca825
rxu <lpeter82218065@gmail.com>
2014-01-11 11:18:36 +0800

    InputReader OutputReader

--- algoLib/sai/Utils/IOUtils.java
+++ algoLib/sai/Utils/IOUtils.java
@@ -23,4 +23,12 @@ public class IOUtils {
         }
         return res;
     }
+
+    public static void readIntArray(InputReader in, int size, int[]... array) {
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < array.length; ++j) {
+                array[j][i] = in.readInt();
+            }
+        }
+    }
 }
